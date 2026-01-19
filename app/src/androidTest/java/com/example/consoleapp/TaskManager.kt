38d63720158c04aa1c651bb01b7a7d768f2821f9
ezgigokay <ezgikucuk1994@gmail.com>
2026-01-19 11:15:42 +0100

    new functionality added, code clened up

--- app/src/androidTest/java/com/example/consoleapp/TaskManager.kt
+++ app/src/androidTest/java/com/example/consoleapp/TaskManager.kt
@@ -1,17 +1,27 @@
 package com.example.consoleapp
 
+//Defining task data class
 data class Task(
-    val taskId: Int,
+    val Id: Int,
     val title: String,
+    var taskStatus: TaskStatus = TaskStatus.TODO
 )
 
+//Defining status options of tasks
+enum class TaskStatus {
+    TODO,
+    DONE
+}
+
+//Defining user interaction options
 enum class SelectOptions(val option: String) {
     ADD_TASK("1"),
     COMPLETE_TASK("2"),
     LIST_ALL_COMPLETED_TASKS("3"),
-    SEARCH_TASK ("4"),
-    LIST_ALL_TASKS("5"),
-    EXIT ("6")
+    LIST_ALL_TASKS("4"),
+    SEARCH_TASK("5"),
+    DELETE_TASK("6"),
+    EXIT("7")
 }
 
 fun printWelcome() {
@@ -20,85 +30,61 @@ fun printWelcome() {
     println("========================")
 }
 
-fun main() {
-    val taskList = mutableListOf<Task>()
-    val finishedTaskList = mutableListOf<Task>()
-    val totalTasks = mutableListOf<Task>()
-    var isRunning = true
-    var nextIdForTaskList = 1
-    var nextIdForWholeList = 1
+//Defining main source of truth
+val taskList = mutableListOf<Task>()
 
+var isRunning = true
+var nextIdForTaskList = 1
+
+fun main() {
+
+//Starting the loop for execution
     while (isRunning) {
 
-        if (totalTasks.isEmpty()) {
+        if (taskList.isEmpty()) {
             printWelcome()
         }
 
-        println("\nOptions: (1) Add  (2) Complete  (3) List All Completed Tasks   (4) Search Task By Name   (5) List All the Tasks   (6) Exit")
+        println("\nOptions: (1) Add  (2) Complete  (3) List All Completed Tasks  (4) List All the Tasks  (5) Search Task By Name  (6) Delete  (7) Exit")
         print("Choose an option: ")
 
         when (readLine()) {
-
+            //Adding task
             SelectOptions.ADD_TASK.option -> {
-                print("Enter title: ")
-                val title = readLine().toString()
-                taskList.add(Task(nextIdForTaskList++, title))
-                totalTasks.add(Task(nextIdForWholeList++, title))
-                println("Task added")
-                for (task in taskList) {
-                    println("${task.taskId}.[ ] ${task.title}")
-                }
+                addTask()
+                printTasks()
             }
-
+            //Completing selected task by id
             SelectOptions.COMPLETE_TASK.option -> {
                 print("Choose task by ID to complete: ")
-                var idInput = readLine()?.toIntOrNull()
-                var task = taskList.find { it.taskId == idInput }
-                taskList.remove(task)
+                val idInput = readLine()?.toIntOrNull()
+                val task = taskList.find { it.Id == idInput }
                 if (task != null) {
-                    finishedTaskList.add(task)
-                    println("${task.taskId}. [✓] ${task.title}")
-                } else println("Task is not exist")
+                    task.taskStatus = TaskStatus.DONE
+                    println("${task.Id}.[✓] ${task.title} marked as complete.")
+                } else {
+                    println("Error: Task with ID '$idInput' not found.")
+                }
             }
-
+            //Listing all completed tasks
             SelectOptions.LIST_ALL_COMPLETED_TASKS.option -> {
                 println("Listing all finished tasks")
-                if (finishedTaskList.isEmpty()) {
-                    println("\nNo completed tasks.")
-                } else {
-                    for (task in finishedTaskList) {
-                        println("${task.taskId}.[✓] ${task.title}")
-                    }
-                }
-
+                filterAndPrintCompletedTasks()
             }
-
-            SelectOptions.SEARCH_TASK.option -> {
-                print("Enter title: ")
-                val searchedInput = readLine()
-                var result = (totalTasks).find { it.title == searchedInput.toString() }
-                if (result in (totalTasks)) {
-                    if (result in taskList) {
-                        println("${result?.taskId}. [ ] ${result?.title}")
-                    } else if (result in finishedTaskList) {
-                        println("${result?.taskId}. [✓] ${result?.title}")
-                    } else {
-                        println("There is no task with given name..")
-                    }
-                }
-            }
-
+            //Listing all tasks with their statuses
             SelectOptions.LIST_ALL_TASKS.option -> {
                 println("Listing all the tasks")
-                for (task in totalTasks) {
-                    if (task in taskList) {
-                        println("${task.taskId} [ ] ${task.title}")
-                    } else if (task in finishedTaskList) {
-                        println("${task.taskId} [✓] ${task.title}")
-                    }
-                }
+                printTasks()
             }
-
+            //Searching task by name
+            SelectOptions.SEARCH_TASK.option -> {
+                searchTask()
+            }
+            //Deleting task by Id
+            SelectOptions.DELETE_TASK.option -> {
+                deleteTask()
+            }
+            //Exiting
             SelectOptions.EXIT.option -> {
                 println("Exiting..")
                 isRunning = false
@@ -107,3 +93,58 @@ fun main() {
     }
 }
 
+private fun addTask() {
+    print("Enter title: ")
+    val title = readLine() ?: ""
+    taskList.add(Task(getNewId(), title))
+    println("Task added")
+}
+
+private fun getNewId(): Int {
+    return nextIdForTaskList++
+}
+
+
+private fun printTasks(specificTask: List<Task>? = taskList) {
+    if (specificTask != null) {
+        for (task in specificTask) {
+            val check = if (task.taskStatus == TaskStatus.DONE) "✓" else " "
+            println("${task.Id}.[$check] ${task.title}")
+        }
+    }
+}
+
+private fun searchTask() {
+    print("Enter title: ")
+    val searchedInput = readLine().toString()
+    taskList.filter { it.title.contains(searchedInput, ignoreCase = true) }.let {
+        if (it.isNotEmpty()) {
+            printTasks(it)
+        } else (println("There is no task with searched title.."))
+    }
+}
+
+private fun filterAndPrintCompletedTasks() {
+    taskList.filter { it.taskStatus == TaskStatus.DONE }
+        .let { completedTasks ->
+            if (completedTasks.isEmpty()) {
+                println("There is no completed task..")
+            } else {
+                for (task in completedTasks) {
+                    println("${task.Id}.[✓] ${task.title}")
+                }
+            }
+        }
+}
+
+private fun deleteTask() {
+    print("Pls enter task Id to delete: ")
+    val enteredId = readln().toIntOrNull()
+    val removed = taskList.removeIf { it.Id == enteredId }
+    if (removed) {
+        println("$enteredId. task deleted successfully")
+        printTasks()
+    } else {
+        println("Task with ID $enteredId not found.")
+    }
+}
